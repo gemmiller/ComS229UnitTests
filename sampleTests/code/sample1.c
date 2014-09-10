655971c5b4c38daca14b7e4b16875e6eddbd4493
Geoffrey Miller <gemiller@iastate.edu>
2014-09-10 11:04:02 -0700

    added sample project

---
+++ sampleTests/code/sample1.c
@@ -0,0 +1,19 @@
+#include "sample1.h"
+
+int isPositive(int num){
+	if(num>0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+int isNegative(int num){
+	if(num<0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
---
+++ sampleTests/code/sample1.h
@@ -0,0 +1,7 @@
+#ifndef __SAMPLE1_H__
+#define __SAMPLE1_H__
+
+int isPositive(int);
+int isNegative(int);
+
+#endif
